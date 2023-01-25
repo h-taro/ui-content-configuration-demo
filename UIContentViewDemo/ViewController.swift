5aab556a840ea1be0e545e099c2d5cba69347c6d
h-taro <t.hiraishi@bravesoft.co.jp>
2023-01-25 15:06:09 +0900

    Initial Commit

---
+++ UIContentViewDemo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  UIContentViewDemo
+//
+//  Created by 平石　太郎 on 2023/01/25.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
